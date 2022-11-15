2731e781784b730be6500caccaf7581d2ca4f3fb
Jonas Natadiredja <jonas.natadiredja@gmail.com>
2022-11-15 15:08:53 +0100

    My First Website - Thymeleaf

--- src/main/java/be/vdab/luigi/controllers/IndexController.java
+++ src/main/java/be/vdab/luigi/controllers/IndexController.java
@@ -1,21 +1,27 @@
 package be.vdab.luigi.controllers;
 
+import be.vdab.luigi.domain.Adres;
+import be.vdab.luigi.domain.Persoon;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 
-@RestController
+@Controller
 @RequestMapping("/")
 class IndexController {
     @GetMapping
-    public String index() {
-        String morgenOfMiddag = LocalDateTime.now().getHour() < 12 ? "morgen" : "middag";
-        return "<!doctype html><html><title>Hallo</title><body>Goede "
-                + morgenOfMiddag +
-        "</body></html>";
+    public ModelAndView index() {
+        String morgenOfMiddag = LocalTime.now().getHour() < 12 ? "morgen" : "middag";
+        ModelAndView modelAndView = new ModelAndView("index", "moment", morgenOfMiddag);
+        modelAndView.addObject("zaakvoerder", new Persoon("Luigi", "Peperone", 7, true, LocalDate.of(1966, 1, 31),
+                new Adres("Grote markt", "3", 9700, "Oudernaarde")));
+        return modelAndView;
 
     }
 }
